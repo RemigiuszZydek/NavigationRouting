707ec017ced2d36e04722d223b47070dbe212ca5
RemigiuszZydek <zyduremik@gmail.com>
2024-04-16 21:18:00 +0200

    more tailwind and react-icons

--- navigation/src/App.js
+++ navigation/src/App.js
@@ -1,24 +1,34 @@
 import Button from "./Button";
+import { GoBell, GoDownload, GoDatabase } from "react-icons/go";
 
 function App() {
 	return (
 		<div>
 			<div>
-				<Button primary secondary>
-					Buy Now
+				<Button primary>
+					<GoBell></GoBell>Buy Now
 				</Button>
 			</div>
 			<div>
-				<Button>See Deal!</Button>
+				<Button secondary outline>
+					<GoDownload></GoDownload>See Deal!
+				</Button>
 			</div>
 			<div>
-				<Button>Hide Ads!</Button>
+				<Button warning>
+					{" "}
+					<GoDatabase></GoDatabase>Hide Ads!
+				</Button>
 			</div>
 			<div>
-				<Button>Return ME</Button>
+				<Button danger outline rounded>
+					Return ME
+				</Button>
 			</div>
 			<div>
-				<Button>Something</Button>
+				<Button success rounded>
+					Something
+				</Button>
 			</div>
 		</div>
 	);
